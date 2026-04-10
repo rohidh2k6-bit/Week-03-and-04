ad00d2e4c8898516c40419b6a951c3a2eeffe66a
rohidh2k6-bit <rohidh2k6@gmail.com>
2026-04-10 09:15:12 +0530

    Problem 6 completed

--- App/src/week3and4.java
+++ App/src/week3and4.java
@@ -1,30 +1,38 @@
 public class week3and4 {
 
-    static int linearSearch(String[] arr, String target) {
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i].equals(target)) return i;
-        }
-        return -1;
-    }
-
-    static int binarySearch(String[] arr, String target) {
-        int l = 0, r = arr.length - 1;
+    static int floor(int[] arr, int target) {
+        int res = -1, l = 0, r = arr.length - 1;
 
         while (l <= r) {
             int m = (l + r) / 2;
 
-            if (arr[m].equals(target)) return m;
-            else if (arr[m].compareTo(target) < 0) l = m + 1;
-            else r = m - 1;
+            if (arr[m] <= target) {
+                res = arr[m];
+                l = m + 1;
+            } else r = m - 1;
         }
-        return -1;
+        return res;
+    }
+
+    static int ceil(int[] arr, int target) {
+        int res = -1, l = 0, r = arr.length - 1;
+
+        while (l <= r) {
+            int m = (l + r) / 2;
+
+            if (arr[m] >= target) {
+                res = arr[m];
+                r = m - 1;
+            } else l = m + 1;
+        }
+        return res;
     }
 
     public static void main(String[] args) {
 
-        String[] arr = {"accA", "accB", "accB", "accC"};
+        int[] arr = {10, 25, 50, 100};
 
-        System.out.println("Linear: " + linearSearch(arr, "accB"));
-        System.out.println("Binary: " + binarySearch(arr, "accB"));
+        System.out.println("Floor: " + floor(arr, 30));
+        System.out.println("Ceil: " + ceil(arr, 30));
     }
 }
